144e79940eb6f3096eec664d3d44a848443a639f
Harnekgithub <88690338+Harnekgithub@users.noreply.github.com>
2022-02-08 17:34:00 -0700

    Final Version of all files for the porject

--- Code/contracts/app.py
+++ Code/contracts/app.py
@@ -1,10 +1,12 @@
 import os
 import json
 from unittest.util import _MAX_LENGTH
+from numpy import require
 from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+import web3
 
 load_dotenv()
 
@@ -40,31 +42,27 @@ def load_contract():
 # Load the contract
 contract = load_contract()
 
-accounts = w3.eth.accounts
-_account = st.selectbox("Select Account", options=accounts)
-
-# testing who is the owner
-st.write(contract.functions.getOwner().call())
-
+#col1, col2 = st.columns([5,2])
+#with col1:
+st.header("Create a Will")
 ################################################################################
-# Register New Artwork
+# Register New Will
 ################################################################################
-st.title("New Will")
-#accounts = w3.eth.accounts
+
 firstName = st.text_input("Enter the First Name of the owner of the will")
 lastName = st.text_input("Enter the Last Name of the owner of the will")
-ssn = st.text_input("Enter the Social Security Number of the owner of the will format ", value = "XXX-XX-XXXX", type="password", max_chars=11)
+ssn = st.text_input("Enter the Social Security Number of the owner of the will format XXX-XX-XXXX ", value = "XXX-XX-XXXX", type="password", max_chars=11)
 dob = st.date_input("Enter the Date of Birth of the owner of the will")
 walletAddress = st.text_input("The Wallet Address of the owner of the will")
+execWallet = st.text_input("The Wallet Address of the Executor of the will")
+execKey = st.text_input("The a secret Key for the Executor to as a password to the will")
 if st.button("Register will"):
     tx_hash = contract.functions.setOwnerdata(firstName, lastName, str(ssn), str(dob),
-     walletAddress).transact({'from': walletAddress, 'gas': 1000000})
+    walletAddress, execWallet, execKey).transact({'from': walletAddress, 'gas': 1000000})
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     st.write("Transaction receipt mined:")
     st.write(dict(receipt))
-st.markdown("---")
-
-
+    st.markdown("---")
 
 
 ################################################################################
@@ -75,46 +73,95 @@ st.title("Register Beneficiary")
 st.subheader("!! Add all Beneficiaries before adding the list of Beneficiaries to the will !!")
 b_firstName = st.text_input("Enter the First Name of the Beneficiary")
 b_lastName = st.text_input("Enter the Last Name of the Beneficiary")
-b_ssn = st.text_input("Enter the Social Security Number of the Beneficiary ", value = "XXX-XX-XXXX", type="password", max_chars=11)
+b_ssn = st.text_input("Enter the Social Security Number of the Beneficiary format XXX-XX-XXXX ", value = "XXX-XX-XXXX", type="password", max_chars=11)
 b_dob = st.date_input("Enter the Date of Birth of the Beneficiary")
-b_share = st.number_input("Enter the share of the estate", max_value=100)
+b_share = st.number_input("Enter the percentage share of the estate", max_value=100)
 b_walletAddress = st.text_input("The Wallet Address of the Beneficiary")
 
 
 if st.button("Register Beneficiary"):
     tx_hash = contract.functions.setBeneCallInternalFunc(b_firstName, b_lastName, 
-    str(b_ssn), str(b_dob), int(b_share), b_walletAddress).transact({'from': walletAddress, 'gas': 1000000})
+    str(b_ssn), str(b_dob), int(b_share), b_walletAddress).transact({'from': b_walletAddress, 'gas': 1000000})
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     st.write("Transaction receipt mined:")
     st.write(dict(receipt))
+    #contract.functions.removebeneficiariesOfWill().call()
 
 
 st.markdown("---")
 
 # Get the willId to attach the beneificaries list to
 _willId = st.text_input("Enter the Will ID to attach the Beneficiary list")
-if st.button("Add Beneficiary to the will"):
+if st.button("Add Beneficiaries to the will"):
     tx_hash = contract.functions.setBeneInternalFuncCall(int(_willId)).transact({'from': walletAddress, 'gas': 1000000})
     st.write(contract.functions.setBeneInternalFuncCall(int(_willId)).call())
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     st.write("Transaction receipt mined:")
     st.write(dict(receipt))
-    
-    st.markdown("---")
+    st.success('Congratulations, you have succesfully created the will for yourself!')
+    st.balloons()
+st.markdown("---")
 
+
+
+
+
+## Sidebar Code Starts here
 with st.sidebar:
-    _owillId = st.text_input("Enter the Owner Will ID")
+    _owillId = st.text_input("Enter the Owner's Will ID")
+    _owalletAddress = st.text_input("Enter the Will Owner's Wallet Address")
     #_ossn = st.text_input("Enter the SSN")
     st.markdown("Click on the buttons below to Review Data")
     if st.button("View Will Owner Data"):
-        ownerData = contract.functions.getWill(int(_owillId)).call()
+        ownerData = contract.functions.getWill(int(_owillId), _owalletAddress).call()
         st.write("Owner Data:")
         st.write(ownerData)
         
  
     _bwillId = st.text_input("Enter the Beneficiary Will ID")
-    _bssn = st.text_input("Enter the Beneficiary SSN")
+    _bssn = st.text_input("Enter the Beneficiary SSN format XXX-XX-XXXX ")
     if st.button("View Beneficiary Info"):
         beneData = contract.functions.getBeneficiaries(int(_bwillId), _bssn).call()
-        st.write("Beneficiary Data:")
-        st.write(beneData)
+        st.write(f"Beneficiary Data: {beneData}")
+
+    _execwallet = st.text_input("Enter the Executor's Wallet Address")
+    _execKey = st.text_input("Enter the Executor's Secret Key")
+    _willId = st.text_input("Enter the Will ID")
+    _ownerWallet = st.text_input("Enter the Will's Wallet Address")
+    _assetPKey =st.text_input("Enter the Asset's Private Key to transfer funds")
+
+
+    if st.button("Assert Death and Execute the will"):
+        _payees = contract.functions.assertDeath(_execwallet,_execKey, int(_willId), _ownerWallet).call()
+        _assetBalance = w3.eth.get_balance(_ownerWallet)
+        st.write(_assetBalance)   
+        for _payee in _payees:
+
+            _to = _payee[-1]
+            _share = _payee[-2]
+            st.write(_payee)
+            nonce = w3.eth.getTransactionCount(_ownerWallet)
+            st.write(_assetBalance)
+            st.write(_share)
+            gas = 300000
+            value  = w3.fromWei(_assetBalance * ((_share-.02)/100), 'ether')  - w3.fromWei(int(gas), 'ether')
+            #value  = _assetBalance * (_share/100)
+            st.write(value)
+            tx = {
+                'nonce': nonce,
+                'to': _to,
+                'value': w3.toWei(value, 'ether'),
+                'gas': gas,
+                'gasPrice': w3.toWei('50', 'gwei'),   
+                'chainId':1337  
+            }
+            signed_tx = w3.eth.account.signTransaction(tx, _assetPKey)
+            tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+            st.write("Transaction receipt mined:")
+            st.write(dict(receipt)) 
+
+                    
+        #    st.success('You have succesfully executed the will!')
+       # else:
+          #  st.write("You are not authorized to execute the will")
--- Code/contracts/compiled/initiator_of_the_will.sol
+++ Code/contracts/compiled/initiator_of_the_will.sol
@@ -1,30 +1,41 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.17;
+// OpenZeppelin Contracts v4.4.1 (finance/PaymentSplitter.sol)
+pragma solidity >=0.4.21;
 pragma experimental ABIEncoderV2;
-// import "./will.sol";
-//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/utils/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
+
+
+
 
 
 // Create owner of the will
-contract owner {
+contract cryptoWill {
     event setOwnerData( uint date, address indexed from );
     event setBeneficiaryToWill( uint date, address indexed from );
     event UpdateBeneficiaryShare( uint date, address indexed from );
     event UpdateBeneficiaryWallet( uint date, address indexed from );
     event setBeneficiaryData( uint date, address indexed from );
+    
+    using SafeMath for uint;
+
+
 
     // Will index initialization
-    //uint256 willId=0;
-    // mapping as struct so that it can be returned back
+    uint256 _sharesRemaing=100;
 
+    // mapping as struct so that it can be returned back
     struct willMap{
-        uint256 numOfWills;
+        uint256 willNum;
         // Create collection on wills        
         mapping(uint256 => owner_of_the_will) wills;
     }
     willMap internal allWills;
 
-    // Struct to contain the data of the owner of the will
+    // Struct for owner of the will data
     struct owner_of_the_will{
         string o_firstName;
         string o_lastName;
@@ -32,15 +43,19 @@ contract owner {
         string o_dob;
         address o_wallet_address;
         beneficiary[] beneficiaries;
+        address execWallet; // this is person who will assert death
+        string execKey; // this is secret phrase to be able to assert death
+        bool o_pol; // proof of life to be set to true when setting up the will
     }
-    // Struct to contain the data of the owner of the Beneficiaries
+
+    // Struct of the Beneficiaries data 
     struct beneficiary{
         string b_firstName;
         string b_lastName;
         string b_ssn;
         string b_dob;
         uint b_share;
-        address b_wallet_address;
+        address payable b_wallet_address;
     }
     // Create a list of beneficiaries to add to the will 
     beneficiary[] public beneficiariesOfWill;
@@ -49,7 +64,7 @@ contract owner {
     owner_of_the_will public wOwner;
     
     // declare the will owner's address variable as a state variable
-    address willOwner;
+    address public willOwner;
 
     // this modifier will make sure that only Owner of the will will have access to the functions
     modifier onlyOwner {
@@ -57,7 +72,10 @@ contract owner {
     _;
     }
     // constructor for the owner contract
-    constructor() public { willOwner = msg.sender;  } 
+    constructor() payable {
+        willOwner = msg.sender;
+    }
+
 
     // Get owner data
     function getOwner() public view returns( address )
@@ -70,7 +88,10 @@ contract owner {
                           string memory _lastName, 
                           string memory _ssn, 
                           string memory _dob, 
-                          address _walletAddress) public  
+                          address _walletAddress,
+                          address _execWallet,
+                          string memory _execKey
+                          ) public  
     {
       //owner_of_the_will owner;
         wOwner.o_firstName = _firstName;
@@ -78,11 +99,13 @@ contract owner {
         wOwner.o_ssn = _ssn;
         wOwner.o_dob = _dob;
         wOwner.o_wallet_address = _walletAddress;
-
+        wOwner.execWallet = _execWallet;
+        wOwner.execKey = _execKey;
+        wOwner.o_pol = true;
         // Increment the willId for this will
-        uint256 willId = allWills.numOfWills++;
+        uint256 willId = allWills.willNum++;
         
-        // Create the mappping of numOfWills to the wills
+        // Create the mappping of willNum to the wills
         allWills.wills[willId] = wOwner;
         
         //log the transaction
@@ -90,16 +113,15 @@ contract owner {
     }
        
     // Get owner data
-    function getWillData(uint256 _willId, address _owalletAddress) internal view returns(owner_of_the_will storage )
+    function getWillData(uint _willId, address _owalletAddress) internal view returns(owner_of_the_will storage )
     {
         require(_owalletAddress == allWills.wills[_willId].o_wallet_address, "You are not Authorized to access this account");
         return(allWills.wills[_willId]);
     }
 
     // Use this public function to call the Internal function to get the the will
-    function getWill(uint256 _willId, address _owalletAddress) public view returns (owner_of_the_will memory){
-        return(getWillData(_willId, _owalletAddress));
-    }
+    function getWill(uint _willId, address _owalletAddress) public view returns (owner_of_the_will memory){
+        return(getWillData(_willId, _owalletAddress));}
 
     // This function set the values passed to it in the state variables for the beneficiarOfWill  
     function setBeneficiarydata(string memory _firstName, 
@@ -107,9 +129,11 @@ contract owner {
                                 string memory _ssn, 
                                 string memory _dob, 
                                 uint _share, 
-                                address _walletAddress) internal 
+                                address payable _walletAddress) internal 
     {
         beneficiary memory _beneficiaries;
+        // check if there are enough shares to be allocated to the new beneficiary
+        //if (_sharesRemaing >= _share)
         _beneficiaries.b_firstName = _firstName;
         _beneficiaries.b_lastName = _lastName;
         _beneficiaries.b_ssn = _ssn;
@@ -117,6 +141,7 @@ contract owner {
         _beneficiaries.b_share = _share;
         _beneficiaries.b_wallet_address = _walletAddress;
         beneficiariesOfWill.push(_beneficiaries);
+        //_sharesRemaing -= _share;
         emit setBeneficiaryData(block.timestamp, msg.sender );
     }
 
@@ -126,34 +151,52 @@ contract owner {
                                      string memory _ssn, 
                                      string memory _dob, 
                                      uint _share, 
-                                     address _walletAddress) public{
+                                     address payable _walletAddress) public{
         setBeneficiarydata(_fname, _lname, _ssn,  _dob, _share, _walletAddress) ;
     }    
 
-    // update will owner's wallet address
-    function updateOwnersWalletAddress(address _walletAddress) internal onlyOwner{
-        owner_of_the_will storage newOwner = wOwner;
-        newOwner.o_wallet_address = _walletAddress;
-        emit setBeneficiaryToWill(block.timestamp, msg.sender );
-
-    }
+   
     // update will beneficiaries once all the beneficiaries are added to the array
     function setBeneficiaries_in_will(uint256 _willId) internal {
         willMap storage will = allWills;
         will.wills[_willId].beneficiaries = beneficiariesOfWill;
         delete beneficiariesOfWill;
     }
+    // This function clears out the array of beneficiaries for the next will
+    function removebeneficiariesOfWill() public
+        {
+            for(uint i; i<beneficiariesOfWill.length-1; i++){
+                beneficiariesOfWill.pop();
+
+            }
+        }
+
     // Use this public function to call the Internal function to set the beneficiaries in the will
     function setBeneInternalFuncCall(uint256 _willId) public {
         setBeneficiaries_in_will(_willId);
      }    
-    // get all beneficiaries
-   /* function getBeneficiaries(uint256 _willId, string memory _ossn) public view returns(beneficiary[] memory){
-        for(uint256 i; i < allWills.wills[_willId].beneficiaries.length; i++){
-            if(compareTwoStrings(allWills.wills[_willId].o_ssn, _ossn)){
-                return(allWills.wills[_willId].beneficiaries);
-            }
-        }*/
+     // This function is called when the owner of the will is deceased and the executor of the will asserts that the person has indeed died
+     // this will also distribute the assets according to the will to heirs
+    function assertDeath(address _execwallet, string memory _execKey, uint256 _willId, address _ownerWallet) public returns(beneficiary[] memory) { 
+        // Validate executor identity , uint256 _assetBalance
+        require(allWills.wills[_willId].o_pol == true, "Person already deceased and the Will has been Executed"); // Check for Proof Of life
+        require(_ownerWallet == allWills.wills[_willId].o_wallet_address, "You are not Authorized to access this account");
+        require(compareTwoStrings(_execKey, allWills.wills[_willId].execKey), "You are not Authorized to access this account");
+        require(_execwallet == allWills.wills[_willId].execWallet, "You are not Authorized to access this account");
+        
+        // Check if there are any payees in the will
+        require(allWills.wills[_willId].beneficiaries.length > 0, "PaymentSplitter: no payees");
+        
+       allWills.wills[_willId].o_pol = false;
+       return (allWills.wills[_willId].beneficiaries);
+    }
+
+     // compares two strings
+    function compareTwoStrings(string memory string1, string memory string2) public pure returns (bool)
+    {
+        return keccak256(abi.encodePacked(string1)) == keccak256(abi.encodePacked(string2));
+    }
+
     // get all beneficiaries
     function getBeneficiaries(uint256 _willId, string memory _bssn) public view returns (string memory _fname, 
                                      string memory _lname, 
@@ -172,7 +215,31 @@ contract owner {
             }
         }        
     }
-    // Update a beneficiary's share
+
+
+ /*   
+     //Code below is for future use NOT ALL is working
+    // get all beneficiaries
+   function getAllBeneficiaries(uint256 _willId, string memory _ossn) public view returns(beneficiary[] memory)
+   {
+        for(uint256 i; i < allWills.wills[_willId].beneficiaries.length; i++)
+        {
+            if(compareTwoStrings(allWills.wills[_willId].o_ssn, _ossn))
+            {
+                return(allWills.wills[_willId].beneficiaries);
+            }
+        }
+   }
+
+  // update will owner's wallet address
+    function updateOwnersWalletAddress(address _walletAddress) internal onlyOwner{
+        owner_of_the_will storage newOwner = wOwner;
+        newOwner.o_wallet_address = _walletAddress;
+        emit setBeneficiaryToWill(block.timestamp, msg.sender );
+
+    }
+ 
+  // Update a beneficiary's share
     function updateBeneficiaryShare(string memory _ssn, uint _share) public  onlyOwner returns(bool){
         beneficiary[] storage newBenef = beneficiariesOfWill;
         for (uint i=0; i < newBenef.length; i++)
@@ -188,7 +255,7 @@ contract owner {
     }
     
     // Let beneficiary update their wallet address
-    function updateBeneficiaryWallet(string memory _ssn, address _walletAddress) public onlyOwner returns(bool) {
+    function updateBeneficiaryWallet(string memory _ssn, address payable _walletAddress) public onlyOwner returns(bool) {
         beneficiary[] storage newBenef = beneficiariesOfWill;
         for (uint i=0; i < newBenef.length; i++)
         {
@@ -200,11 +267,6 @@ contract owner {
             }
         }
         return false;
-    }
+    }*/
 
-    // compares two strings
-    function compareTwoStrings(string memory string1, string memory string2) public pure returns (bool)
-    {
-        return keccak256(abi.encodePacked(string1)) == keccak256(abi.encodePacked(string2));
-    }
 }
